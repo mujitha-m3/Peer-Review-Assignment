ae66df0e9bb8c16c9db7bcfee72375707f92080f
Mujitha <mujitha.m3@gmail.com>
2023-09-19 22:10:34 +0300

    adding comments

--- Login.java
+++ Login.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] Args){
         Scanner scan = new Scanner(System.in);
-
+        
+        // Collecting the data from user
         System.out.println("Enter your first name:");
         String firstName = scan.nextLine();
 
@@ -13,9 +14,11 @@ public class Login {
         System.out.println("Enter your business domain:");
         String domain = scan.nextLine();
 
+        // Check if firstName and lastName is empty or not 
         if(firstName.isEmpty() || lastName.isEmpty()){
             System.out.println("Error! First and/or last name is missing");
         } else{
+            // Calling the method and stored the genarated data into variable
            String email = GenerateEmail(firstName, lastName, domain);
            String username = GenerateUsername(firstName, lastName);
 
@@ -24,7 +27,7 @@ public class Login {
         }
         scan.close();
     }
-
+ 
     public static String GenerateEmail(String firstName, String lastName, String domain){
         String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + domain.toLowerCase();
         return email;
