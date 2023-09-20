ad627266e134c66f3e25a81d85d44d6f2958d078
Mujitha <mujitha.m3@gmail.com>
2023-09-20 19:49:46 +0300

    adding more comment into code

--- Login.java
+++ Login.java
@@ -4,7 +4,7 @@ public class Login {
     public static void main(String[] Args){
         Scanner scan = new Scanner(System.in);
         
-        // Collecting the data from user
+        // Ask the user to input their first name, last name, and domain.
         System.out.println("Enter your first name:");
         String firstName = scan.nextLine();
 
@@ -14,25 +14,29 @@ public class Login {
         System.out.println("Enter your business domain:");
         String domain = scan.nextLine();
 
-        // Check if firstName and lastName is empty or not 
+        // Check if first name or last name is missing. 
         if(firstName.isEmpty() || lastName.isEmpty()){
             System.out.println("Error! First and/or last name is missing");
         } else{
-            // Calling the method and stored the genarated data into variable
+            // If both names are provided, generate email and username.
            String email = GenerateEmail(firstName, lastName, domain);
            String username = GenerateUsername(firstName, lastName);
 
+            // Print the generated email and username.
            System.out.println("Genarated Email: " + email);
            System.out.println("Genarated Username: " + username);
         }
         scan.close();
     }
- 
+    
+    // Method to generate an email address based on first name, last name, and domain.
     public static String GenerateEmail(String firstName, String lastName, String domain){
         String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + domain.toLowerCase();
         return email;
     }
 
+    /* Method to generate a username based on the first four characters of the first name
+     and the last four characters of the last name.*/ 
     public static String GenerateUsername(String firstName, String lastName){
         String username = firstName.substring(0, 4).toLowerCase() + lastName.substring(0, 4).toLowerCase();
         return username; 
